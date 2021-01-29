31d92457d192c15e906c363c12b3be1a0960e28b
racmalin <racmalin@yahoo.com>
2021-01-29 10:06:29 -0700

    Done.

--- db/migrate/20210129164611_create_users.rb
+++ db/migrate/20210129164611_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-      t.decimal :account_balance
+      # t.decimal :account_balance
     end
 
   end
